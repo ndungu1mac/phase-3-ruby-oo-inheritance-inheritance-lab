6c59adec334ca060ffe495628e27b6d88d1d2268
ndungu1mac <macgeorgendungu@gmail.com>
2022-11-04 23:14:57 +0300

    Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative 'user'
+class Student < User
+    attr_reader :knowledge , :learn
+    def initialize
+        @knowledge = []
+    end
+   
+    def learn(sbj)
+        self.knowledge << sbj
+    end
 
+  
 end
